3bbfbb9ca5c7e06509c5abc569ac0ac30637708f
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2021-08-04 17:38:22 -0500

    css-prod

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,46 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React, {useEffect, useState, useContext} from 'react';
 import Layout from '../components/layout/Layout';
-
-const Heading = styled.h1`
-  color: red;
-`
+import { FirebaseContext } from '../firebase';
+import DetallesProducto from '../components/layout/DetallesProducto';
 
 export default function Home() {
+
+  const [productos, setProductos] = useState([]);
+
+  const {firebase} = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(manejarSnapshot)
+    }
+    obtenerProductos();
+  }, []);
+
+  function manejarSnapshot( snapshot ) { 
+    const productos = snapshot.docs.map(doc => { // Manera de traer el registro completo
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    });
+    setProductos(productos);
+  } 
+
   return (
     <div>
       <Layout>
-        <h1>Inicio</h1>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <div className="bg-white">
+              {productos.map(producto =>(
+                <DetallesProducto
+                  key={producto.id}
+                  producto={producto}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
       </Layout>
     </div>
   )
